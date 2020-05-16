1f7768fe00eb3770185c000578260ebd81a1c0f1
Andrew Reid <areid.sj@gmail.com>
2020-05-16 10:38:47 -0600

    Standardize model and model test files

--- spec/models/invoice_items_spec.rb
+++ spec/models/invoice_items_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe InvoiceItem, type: :model do
   describe 'relationships' do
     it { should belong_to :invoice }
     it { should belong_to :item }
-
   end
 
   describe 'validations' do
