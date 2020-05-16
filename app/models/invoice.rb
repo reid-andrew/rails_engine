818c95681ef561976bbedfbc912ccacabf2723f1
Andrew Reid <areid.sj@gmail.com>
2020-05-16 10:37:48 -0600

    Add transaction model and tests

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,6 +2,7 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
   has_many :invoice_items
+  has_many :transactions
 
   validates :status, presence: true
 end
--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Invoice, type: :model do
     it { should belong_to :merchant }
     it { should belong_to :customer }
     it { should have_many :invoice_items }
+    it { should have_many :transactions }
   end
 
   describe 'validations' do
