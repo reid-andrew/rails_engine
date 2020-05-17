72773bb98b6e13198e8aedc9660deed2c9ad9013
Andrew Reid <areid.sj@gmail.com>
2020-05-17 12:43:15 -0600

    Write and pass additional tests for find multiple items

--- spec/requests/api/v1/finds/find_multiple_items_endpoint_spec.rb
+++ spec/requests/api/v1/finds/find_multiple_items_endpoint_spec.rb
@@ -34,4 +34,79 @@ RSpec.describe 'Find API Endpoints - ', type: :request do
     expect(items["data"][1]["attributes"]["description"]).to eq(expected_2.description)
     expect(items["data"][2]["attributes"]["description"]).to eq(expected_3.description)
   end
+
+  it 'finds multiple items by description' do
+    create_list(:item, 3)
+    expected_1 = Item.create(name: Faker::Appliance.unique.equipment,
+                             description: "I'm not working for the clampdown",
+                             unit_price: Faker::Number.positive,
+                             merchant_id: @merchant.id)
+    create_list(:item, 3)
+    expected_2 = Item.create(name: Faker::Appliance.unique.equipment,
+                             description: "I'll be working all day for me mates",
+                             unit_price: Faker::Number.positive,
+                             merchant_id: @merchant.id)
+    create_list(:item, 3)
+    expected_3 = Item.create(name: Faker::Appliance.unique.equipment,
+                             description: "Working on a dream",
+                             unit_price: Faker::Number.positive,
+                             merchant_id: @merchant.id)
+    create_list(:item, 3)
+
+    get '/api/v1/items/find_all?description=working'
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"].size).to eq(3)
+    expect(items["data"][0]["id"]).to eq(expected_1.id.to_s)
+    expect(items["data"][1]["id"]).to eq(expected_2.id.to_s)
+    expect(items["data"][2]["id"]).to eq(expected_3.id.to_s)
+    expect(items["data"][0]["attributes"]["description"]).to eq(expected_1.description)
+    expect(items["data"][1]["attributes"]["description"]).to eq(expected_2.description)
+    expect(items["data"][2]["attributes"]["description"]).to eq(expected_3.description)
+  end
+
+  it 'finds multiple items by multiple criteria' do
+    create_list(:item, 3)
+    expected_1 = Item.create(name: "Widget",
+                             description: "I'm not working for the clampdown",
+                             unit_price: Faker::Number.positive,
+                             merchant_id: @merchant.id)
+    create_list(:item, 3)
+    expected_2 = Item.create(name: "Gadget",
+                             description: "I'll be working all day for me mates",
+                             unit_price: Faker::Number.positive,
+                             merchant_id: @merchant.id)
+    create_list(:item, 3)
+    expected_3 = Item.create(name: "Sledgehammer",
+                             description: "Working on a dream",
+                             unit_price: Faker::Number.positive,
+                             merchant_id: @merchant.id)
+    create_list(:item, 3)
+
+    get '/api/v1/items/find_all?name=dge&description=working'
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"].size).to eq(3)
+    expect(items["data"][0]["id"]).to eq(expected_1.id.to_s)
+    expect(items["data"][1]["id"]).to eq(expected_2.id.to_s)
+    expect(items["data"][2]["id"]).to eq(expected_3.id.to_s)
+    expect(items["data"][0]["attributes"]["description"]).to eq(expected_1.description)
+    expect(items["data"][1]["attributes"]["description"]).to eq(expected_2.description)
+    expect(items["data"][2]["attributes"]["description"]).to eq(expected_3.description)
+  end
+
+  it 'handles no results provided' do
+    Item.create(name: "Widget",
+                description: "Widgets of the world unite!",
+                unit_price: Faker::Number.positive,
+                merchant_id: @merchant.id)
+
+    get '/api/v1/items/find_all?name=nope'
+    items = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(items["data"]).to eq([])
+  end
 end
