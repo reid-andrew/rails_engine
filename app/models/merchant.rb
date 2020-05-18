117b86d1ebe88f2cae36853765eda0d49c2bf6ff
Andrew Reid <areid.sj@gmail.com>
2020-05-18 15:42:24 -0600

    Pass initial test with mostly sql

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,35 @@ class  Merchant < ApplicationRecord
   has_many :items
 
   validates :name, presence: true
+
+  def self.most_revenue(quantity)
+    sql =  "SELECT m.*, SUM(i.revenue) AS ttl_rev
+            FROM merchants m
+            INNER JOIN (
+                SELECT invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue
+                FROM invoices
+                INNER JOIN invoice_items ON invoice_items.invoice_id = invoices.id
+                INNER JOIN transactions ON transactions.invoice_id = invoices.id
+                WHERE transactions.result = 'success'
+                GROUP BY invoices.id) i
+            ON m.id = i.merchant_id
+            GROUP BY m.id
+            ORDER BY ttl_rev DESC
+            LIMIT(#{quantity});"
+    Merchant.find_by_sql(sql)
+
+    # inv_totals = Invoice.select('invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue')
+    # .joins(:invoice_items, :transactions)
+    # .merge(Transaction.successful)
+    # .group(:id)
+    #
+    # select('merchants.*, SUM(inv_totals.revenue) AS total_revenue')
+    # .joins(:inv_totals)
+    # .group(:id)
+    # .order("total_revenue")
+    # .limit(1)
+    #
+    #
+    # inv_totals = Invoice.select('invoices.*, SUM(invoice_items.quantity * invoice_items.unit_price) AS revenue').joins(:invoice_items, :transactions).merge(Transaction.successful).group(:id)
+  end
 end
--- spec/requests/api/v1/bi_endpoints/merchant_with_most_revenue_spec.rb
+++ spec/requests/api/v1/bi_endpoints/merchant_with_most_revenue_spec.rb
@@ -3,15 +3,22 @@ require 'rails_helper'
 RSpec.describe 'Business Intelligence API Endpoint - ', type: :request do
   describe 'Merchants with Most Revenue - ' do
     before (:each) do
-      create_list(:invoice_item, 10)
+      inv_items = create_list(:invoice_item, 10)
+      inv_items.each do |item|
+        item.invoice.transactions.create(
+          credit_card_number: Faker::Number.number(digits: 16),
+          credit_card_expiration_date: Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all),
+          result: 'success')
+          # result: ['success', 'failed'].sample)
+      end
     end
 
     it 'finds single merchant' do
       get '/api/v1/merchants/most_revenue?quantity=1'
-      merchants = JSON.parse(response.body)
+      expected = JSON.parse(response.body)
 
       expect(response).to be_successful
-      expect(merchants["data"].size).to eq(1)
+      expect(expected["data"].size).to eq(1)
     end
 
   end
