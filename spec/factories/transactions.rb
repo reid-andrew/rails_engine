9f37daf01c29b0e16976f2ffa2dfc362d0642830
Andrew Reid <areid.sj@gmail.com>
2020-05-18 13:48:56 -0600

    Correct typo in factory setup

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :invoice_item do
+  factory :transaction do
     invoice
     credit_card_number { Faker::Number.number(digits: 16) }
     credit_card_expiration_date { Faker::Time.between_dates(from: Date.today - 1, to: Date.today, period: :all) }
